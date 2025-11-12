6c1f1863ab2a125a3099b3ed7de3224711dd82b4
Csandoval-dev <156733491+Csandoval-dev@users.noreply.github.com>
2025-11-11 19:00:54 -0600

    Cnfiguracion Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBtoTDwv9LDJhGPlv3Ek7zCoexKBftC8Qk",
+    authDomain: "examen1-fea07.firebaseapp.com",
+    projectId: "examen1-fea07",
+    storageBucket: "examen1-fea07.firebasestorage.app",
+    messagingSenderId: "65440682629",
+    appId: "1:65440682629:web:77a238a212c001b8de8fec"
+  }
+};
